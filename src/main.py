cefa8724ddbbe955aa7411fc4eeb87e69d738586
Tim Jones <tim.jones@adlucent.com>
2022-09-30 10:12:37 -0500

    add basic logic for POST request

--- src/main.py
+++ src/main.py
@@ -1,2 +1,9 @@
+import functions_framework
+
+@functions_framework.http
 def slugify(request):
-    return "Hello Test World"
+    if request.method == "POST":
+        name = request.get_json.get('name', 'World')
+        return f"Success: your name is now {name}!"
+    name = request.args.get('name', 'World')
+    return f"Hello {name}"
